0ea56b5b1a86b1e33ee965330c4d74df2391df1d
JOHN <John@gmail.com>
2022-06-12 19:47:38 -0500

    Agragando la funcion de encriptar y asociandola con el boton

--- js/encriptar.js
+++ js/encriptar.js
@@ -1,9 +1,18 @@
-var empezar = "No he añadido nada";
 
+const mensaje = document.querySelector(".mensaje");
+const salida = document.querySelector(".salida");
 
-
-
-
-
-
-
+function btnEncriptar() {
+    const textoEncriptado = encriptar(mensaje.value);
+    salida.value = textoEncriptado;
+}
+function encriptar(stringAEncriptar){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+    stringAEncriptar = stringAEncriptar.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringAEncriptar.includes(matrizCodigo[i][0])) {
+            stringAEncriptar = stringAEncriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return stringAEncriptar;
+}
